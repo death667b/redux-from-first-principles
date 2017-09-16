f54b3ddcf33de6eb7b71d8c59656679f67a80614
death667b <idaniel@spinnys.com>
2017-09-16 14:24:49 +1000

    new 07 redux

---
+++ 07/redux.ts
@@ -0,0 +1,40 @@
+export function createStore(reducer, state = {}) {
+    let subscribers = new Set();
+
+    return {
+        getState: () => state,
+        dispatch: (currentAction: string, value?) => {
+            let newState = reducer(state, currentAction, value);
+            if (newState != state)  {
+                state = newState;
+                if (subscribers) {
+                    subscribers.forEach(fn => fn(newState))
+                }
+            }
+        },
+        subscribe: fn => subscribers.add(fn)
+    }
+}
+
+export function reducer(prevValue, currAction, value = 0) {
+    let newState = 0;
+
+    switch(currAction) {
+        case '@@INIT':
+            newState = 0;
+            break;
+        case 'INCREMENT':
+            newState = prevValue+1;
+            break;
+        case 'DECREMENT':
+            newState = prevValue-1;
+            break;
+        case 'ADD':
+            newState = prevValue + value;
+            break;
+        default:
+            newState = prevValue;
+    }
+    return newState;
+}
+
